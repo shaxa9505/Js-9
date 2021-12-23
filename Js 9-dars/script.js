8fc4f337987cd1f5f235e6fa85a22b1a31c3a4cd
shaxa9505 <87860218+shaxa9505@users.noreply.github.com>
2021-12-23 21:39:03 +0500

    js9
    js9

---
+++ Js 9-dars/script.js
@@ -0,0 +1,68 @@
+
+// const text = document.getElementById("text");
+const div = document.querySelector(".text")
+
+// div.onclick = function () {
+//     div.innerHTML = "manga sichqoncha bilan bitta bosildi";
+    
+    
+//     div.style.cssText = "margin-top: 20p; color: white";
+
+//     div.style.color = "red";
+//     div.style.background = "black";
+//     div.style.height = "50px";
+//     div.style.textAlign = "center";
+
+//     // greeting.style.cssText = "margin: 10px; font-size: 16px; background: black; color: red";
+//     div.className = "save";
+// }
+
+div.ondblclick = function () {
+    div.innerHTML = "manga sichqoncha bilan bitta bosildi";
+    
+    
+    div.style.cssText = "margin-top: 20p; color: white";
+
+    div.style.color = "red";
+    div.style.background = "black";
+    div.style.height = "50px";
+    div.style.textAlign = "center";
+
+    // greeting.style.cssText = "margin: 10px; font-size: 16px; background: black; color: red";
+    div.className = "save";
+}
+
+div.onmouseover = function () {
+    
+    
+    
+    div.style.cssText = "margin-top: 20p; color: white";
+
+    // div.style.color = "red";
+    // div.style.background = "black";
+    // div.style.height = "50px";
+    // div.style.textAlign = "center";
+
+    // greeting.style.cssText = "margin: 10px; font-size: 16px; background: black; color: red";
+    div.className = "save";
+}
+
+div.onmouseout = function () {
+    
+    
+    
+    div.style.cssText = "margin-top: 20p; color: red";
+
+    // div.style.color = "red";
+    // div.style.background = "black";
+    // div.style.height = "50px";
+    // div.style.textAlign = "center";
+
+    // greeting.style.cssText = "margin: 10px; font-size: 16px; background: black; color: red";
+    div.className = "save";
+}
+
+
+
+
+
